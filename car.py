3e358387af18b5952d141b9d7ba0b4a99e249556
uuoog <d2994@naver.com>
2023-07-14 22:54:01 +0900

    packages 수정

--- car.py
+++ car.py
@@ -129,8 +129,6 @@ if __name__ == "__main__":
 #### 키워드 검색 구현기능
 
 komoran = Komoran(userdic="./data/user.dic")
-
-
 tk["contents_tokens"] = tk["content_tokens"].progress_apply(lambda x: eval(x))
 
 def extract_nouns(tokens):
